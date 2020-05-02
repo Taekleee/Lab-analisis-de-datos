5fd5372b779e76f5ef0a5f71cb7608e47c3e85f3
Cata <Catalina.morales.r@usach.cl>
2020-05-02 01:54:36 -0400

    intento de gráfico

--- Lab1/lab1.R
+++ Lab1/lab1.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(ggplot2)
+library(ggpubr)
 #setwd("/Users/cata/Desktop/Lab-analisis-de-datos/Lab1")
 #DATA.FILTER obtiene la tabla de frecuencia de los votos totales, de democratas y republicanos par cada
 #votación
@@ -7,7 +9,6 @@ data.filter<-function(
   data,
   col.names
 ){
-  data2<-filter(data, data$classname == "democrat")
   data2 <- lapply(X=data,FUN = function(m) df <- summary(m))
   data2[1]<-NULL
   data2<-do.call(rbind.data.frame, data2)
@@ -16,11 +17,25 @@ data.filter<-function(
 }
 
 
+#Cálculo de las medidas como varianza, desviación, media y mediana
+#Salida: Una tabla en donde las filas son los casos ( NV, Y , N, ETC) y las columnas las medidas
 medidas<-function(
   data,
+  mean =NULL,
+  sd = NULL,
+  median = NULL,
+  var = NULL
 ){
-  names <- c("classname", "handicappedinfants","waterprojectcostsharing","adoptionofthebudgetresolution","physicianfeefreeze","elsalvadoraid","religiousgroupsinschools","antisatellitetestban","aidtonicaraguancontras","mxmissile","immigration","synfuelscorporationcutback","educationspending","superfundrighttosue","crime","dutyfreeexports","exportadministrationactsouthafrica")
+  for(i in 1:length(data)){
+    mean[i]<-mean(data[,i])
+    sd[i]<-sd(data[,i])
+    median[i]<-median(data[,i])
+    var[i]<-var(data[,i])
+  }
   
+  result<- data.frame("mean" = mean, "sd" = sd, "median" = median,"var" = var)
+  row.names(result)<-c("NV","n","y","DemocratNV","DemocratN","DemocratY","RepublicanNV","RepublicanN","RepublicY")
+  result
 }
 
 #DATA: DATOS ORIGINALES DE LA BASE DE DATOS, CON LOS POLÍTICOS Y SUS VOTOS
@@ -28,11 +43,18 @@ data <- read.csv("house-votes-84.data", header = TRUE, sep = ",",quote = "\"",fi
 colnames(data) <- c("classname", "handicappedinfants","waterprojectcostsharing","adoptionofthebudgetresolution","physicianfeefreeze","elsalvadoraid","religiousgroupsinschools","antisatellitetestban","aidtonicaraguancontras","mxmissile","immigration","synfuelscorporationcutback","educationspending","superfundrighttosue","crime","dutyfreeexports","exportadministrationactsouthafrica")
 democrat<-filter(data, data$classname == "democrat")
 republican<-filter(data, data$classname == "republican")
-data1<-data.filter(data,c("?","n","y"))
-data2 <- data.filter(democrat,c("Democrat ?","Democrat n","Democrat y"))
-data3 <- data.filter(republican,c("Republican ?","Republican n","Republican y"))
+data1<-data.filter(data,c("NV","n","y"))
+data2 <- data.filter(democrat,c("DemocratNV","DemocratN","DemocratY"))
+data3 <- data.filter(republican,c("RepublicanNV","RepublicanN","RepublicanY"))
 
 final <- c(data1,data2,data3)
-final<-do.call(cbind.data.frame, final)
-rownames(final) <- c( "handicappedinfants","waterprojectcostsharing","adoptionofthebudgetresolution","physicianfeefreeze","elsalvadoraid","religiousgroupsinschools","antisatellitetestban","aidtonicaraguancontras","mxmissile","immigration","synfuelscorporationcutback","educationspending","superfundrighttosue","crime","dutyfreeexports","exportadministrationactsouthafrica")
+final<-do.call(rbind.data.frame, final)
+colnames(final) <- c("handicappedinfants","waterprojectcostsharing","adoptionofthebudgetresolution","physicianfeefreeze","elsalvadoraid","religiousgroupsinschools","antisatellitetestban","aidtonicaraguancontras","mxmissile","immigration","synfuelscorporationcutback","educationspending","superfundrighttosue","crime","dutyfreeexports","exportadministrationactsouthafrica")
+row.names(final)<-c("NV","n","y","DemocratNV","DemocratN","DemocratY","RepublicanNV","RepublicanN","RepublicY")
 
+
+ggplot(final[1:2,], aes(x = rownames(final[1:2,]), y = handicappedinfants))+
+  geom_bar(
+    aes(fill =rownames(final[1:2,]) ), stat = "identity", color = "white",
+    position = position_dodge(0.9)
+  )
