4800fc654f46487e0f74aaab3c8a7b5b034ad0d9
Cata <Catalina.morales.r@usach.cl>
2020-06-09 02:47:05 -0400

    pam agregado

--- Lab2/lab2.R
+++ Lab2/lab2.R
@@ -2,6 +2,9 @@ library(cluster)
 library(purrr) 
 library(factoextra)
 library(plyr)
+library(tclust)
+
+
 url = "http://archive.ics.uci.edu/ml/machine-learning-databases/voting-records/house-votes-84.data"
 data <- read.csv(url, header = TRUE, sep = ",",quote = "\"",fill=T)
 colnames(data) <- c("classname", "handicappedinfants","waterprojectcostsharing","adoptionofthebudgetresolution","physicianfeefreeze","elsalvadoraid","religiousgroupsinschools","antisatellitetestban","aidtonicaraguancontras","mxmissile","immigration","synfuelscorporationcutback","educationspending","superfundrighttosue","crime","dutyfreeexports","exportadministrationactsouthafrica")
@@ -16,27 +19,62 @@ data[data=="?"] <- "0"
 data[,2:17] <- data.frame(lapply(data[,2:17], as.numeric))
 #data <- scale(data[,2:17])
 data <- data[,2:17]
+#Se calcula la distancia euclideana, la matriz de similitud y se aplica k-means
 distance <- get_dist(data, method = "euclidean")
-fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"),show_labels=TRUE)
 k2 <- kmeans(data, centers = 3, nstart = 25)
-a <- fviz_cluster(k2, data = data)
-print(a)
+k2.plot <- fviz_cluster(k2, data = data)
+print(k2.plot)
 
-
-
-
-
-
-
-
+#hclust y agnes se ocupan para la agrupación jerárquica
+#agnes también retorna el coeficiente de aglomeración (ac), el cual indica la cantidad de estructura
+#de agrupamiento encontrada (si es más cercano a 1 es más fuerte).
 hc1 <- hclust(distance, method = "complete" )
 plot(hc1, cex = 0.6, hang = -1)
 hc2 <- agnes(data, method = "complete")
-
-# Agglomerative coefficient
 hc2$ac
 
-
+#Se genera el dendograma
 pltree(hc2, hang=-1, cex = 0.6)
 plot(hc2, cex = 0.6, hang = -1, main = "Dendrogram of diana")
 rect.hclust(hc2, k = 3, border = 1:5)
+
+#Número de elementos por cluster
+cantidad<-table(data$cluster)
+
+
+#Se utiliza pam para generar un agrupamiento k-medoids, en el cual cada cluster se encuentra 
+#representado por su medoids, a diferencia de k-means en donde se representan por el centroide.
+#Recibe como entrada los datos, la cantidad de clúster y la medida de similaridad.
+#Medoids: elemento dentro de un cluster cuya distancia (diferencia) promedio entre él y 
+#todos los demás elementos del mismo cluster es lo menor posible.
+#Medoids se ve menos afectado por el ruido o outliers que k-means
+
+
+pam_clusters <- pam(x = data, k = 2, metric = "manhattan")
+fviz_cluster(object = pam_clusters, data = data, ellipse.type = "t",
+             repel = TRUE) +
+  theme_bw() +
+  labs(title = "Resultados clustering PAM") +
+  theme(legend.position = "none")
+#Con pam se pueden ver los outliers en el gráfico, para k-means se calcula
+#la distancia entre cada elemento y su centroide.
+
+centers <- k2$centers[k2$cluster, ]
+distances <- sqrt(rowSums((data - centers)^2))
+outliers <- order(distances, decreasing=T)[1:15]
+print("Votaciones de los outliers:")
+print(data[outliers,])
+
+new.data<-data[outliers*-1,]
+pam_clusters <- pam(x = new.data, k = 3, metric = "manhattan")
+fviz_cluster(object = pam_clusters, data = data, ellipse.type = "t",
+             repel = TRUE) +
+  theme_bw() +
+  labs(title = "Resultados clustering PAM") +
+  theme(legend.position = "none")
+distance <- get_dist(new.data, method = "euclidean")
+fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"),show_labels=TRUE)
+k2 <- kmeans(new.data, centers = 3, nstart = 25)
+k2.plot <- fviz_cluster(k2, data = new.data)
+print(k2.plot)
