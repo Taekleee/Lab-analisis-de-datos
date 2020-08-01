22b2bf370eefe3c63e522910b4b10b27606884cd
Cata <Catalina.morales.r@usach.cl>
2020-08-01 00:05:39 -0400

    terminados

--- Lab2/lab2.R
+++ Lab2/lab2.R
@@ -6,6 +6,7 @@ library(tclust)
 library(gridExtra)
 library(grid)
 library(ggpubr)
+library(NbClust)
 
 url = "http://archive.ics.uci.edu/ml/machine-learning-databases/voting-records/house-votes-84.data"
 data <- read.csv(url, header = TRUE, sep = ",",quote = "\"",fill=T)
@@ -25,9 +26,9 @@ data <- data[,2:17]
 
 ############################### K-MEANS con todos los datos ####################################
 
-#Se calcula la distancia euclideana, la matriz de similitud y se aplica k-means
+#Se calcula el número óptimo de cluster, la distancia euclideana, la matriz de similitud y se aplica k-means
 
-optimal.plot <- fviz_nbclust(data, kmeans, method = "silhouette")
+index.optimal.cluster<- NbClust(data = data, diss = NULL, distance = "euclidean", min.nc = 2, max.nc = 10, method = "kmeans", index = "all", alphaBeale = 0.1)        
 
 distance <- get_dist(data, method = "euclidean")
 fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"),show_labels=TRUE)
@@ -41,7 +42,7 @@ k4 <- kmeans(data, centers = 4, nstart = 25)
 k4.plot <- fviz_cluster(k4, data = data)
 
 
-ggarrange(k2.plot,k3.plot,k4.plot,optimal.plot)
+ggarrange(k2.plot,k3.plot,k4.plot)
 
 ######################## Cluster con datos significativos en reg.log ###########################
 
@@ -55,7 +56,8 @@ distance <- get_dist(reglog, method = "euclidean")
 fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"),show_labels=TRUE)
 
 #Kmeans clustering con 2 y 3 nodos
-optimalLog.plot <- fviz_nbclust(reglog, kmeans, method = "silhouette")
+index.optimal.cluster.log<- NbClust(data = reglog, diss = NULL, distance = "euclidean", min.nc = 2, max.nc = 10, method = "kmeans", index = "all", alphaBeale = 0.1)        
+
 k2 <- kmeans(reglog, centers = 2, nstart = 25)
 k2.plot <- fviz_cluster(k2, data = reglog)
 k3 <- kmeans(reglog, centers = 3, nstart = 25)
@@ -63,25 +65,10 @@ k3.plot <- fviz_cluster(k3, data = reglog)
 k8 <- kmeans(reglog, centers = 8, nstart = 25)
 k8.plot <- fviz_cluster(k8, data = reglog)
 
-ggarrange(optimalLog.plot, k2.plot, k3.plot, k8.plot)
+ggarrange(k2.plot, k3.plot, k8.plot)
 
-#pam clustering con 2 y 3 nodos
-pam_clusters2 <- pam(x = reglog, k = 2, metric = "manhattan")
-pam_clusters3 <- pam(x = reglog, k = 3, metric = "manhattan")
-pamk2.plot <- fviz_cluster(object = pam_clusters2, data = reglog, ellipse.type = "t",
-             repel = TRUE) +
-  theme_bw() +
-  labs(title = "Resultados clustering PAM") +
-  theme(legend.position = "none")
 
-pamk3.plot <- fviz_cluster(object = pam_clusters3, data = reglog, ellipse.type = "t",
-             repel = TRUE) +
-  theme_bw() +
-  labs(title = "Resultados clustering PAM") +
-  theme(legend.position = "none")
-ggarrange(pamk2.plot,pamk3.plot)
-
-#################################################################################################
+######################################DENDOGRAMA###########################################################
 
 #hclust y agnes se ocupan para la agrupación jerárquica
 #agnes también retorna el coeficiente de aglomeración (ac), el cual indica la cantidad de estructura
@@ -100,12 +87,82 @@ rect.hclust(hc2, k = 3, border = 1:5)
 cantidad<-table(data$cluster)
 
 
+#############################Outliers###############################
+centers <- k2$centers[k2$cluster, ]
+distances <- sqrt(rowSums((data - centers)^2))
+outliers1 <- order(distances, decreasing=T)[1:10]
+outliers2 <- order(distances, decreasing=T)[1:50]
+outliers3 <- order(distances, decreasing=T)[1:100]
+
+print("Votaciones de los outliers:")
+print(data[outliers1,])
+print(data[outliers2,])
+
+new.data1<-data[outliers1*-1,]
+new.data2<-data[outliers2*-1,]
+new.data3<-data[outliers3*-1,]
+
+index.optimal.cluster.out1<- NbClust(data = new.data1, diss = NULL, distance = "euclidean", min.nc = 2, max.nc = 10, method = "kmeans", index = "all", alphaBeale = 0.1)        
+index.optimal.cluster.out2<- NbClust(data = new.data2, diss = NULL, distance = "euclidean", min.nc = 2, max.nc = 10, method = "kmeans", index = "all", alphaBeale = 0.1)        
+index.optimal.cluster.out3<- NbClust(data = new.data3, diss = NULL, distance = "euclidean", min.nc = 2, max.nc = 10, method = "kmeans", index = "all", alphaBeale = 0.1)        
+
+
+k3 <- kmeans(new.data1, centers = 2, nstart = 25)
+k3.plot <- fviz_cluster(k3, data = new.data1)
+k4 <- kmeans(new.data2, centers = 2, nstart = 25)
+k4.plot <- fviz_cluster(k4, data = new.data2)
+k7 <- kmeans(new.data3, centers = 2, nstart = 25)
+k7.plot <- fviz_cluster(k7, data = new.data3)
+
+k5 <- kmeans(new.data1, centers = 3, nstart = 25)
+k5.plot <- fviz_cluster(k5, data = new.data1)
+k6 <- kmeans(new.data2, centers = 3, nstart = 25)
+k6.plot <- fviz_cluster(k6, data = new.data2)
+k8 <- kmeans(new.data3, centers = 3, nstart = 25)
+k8.plot <- fviz_cluster(k8, data = new.data3)
+
+ggarrange(k3.plot, k4.plot, k7.plot, k5.plot,k6.plot,k8.plot)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #Se utiliza pam para generar un agrupamiento k-medoids, en el cual cada cluster se encuentra 
 #representado por su medoids, a diferencia de k-means en donde se representan por el centroide.
 #Recibe como entrada los datos, la cantidad de clúster y la medida de similaridad.
 #Medoids: elemento dentro de un cluster cuya distancia (diferencia) promedio entre él y 
 #todos los demás elementos del mismo cluster es lo menor posible.
 #Medoids se ve menos afectado por el ruido o outliers que k-means
+
+#pam clustering con 2 y 3 nodos
+pam_clusters2 <- pam(x = reglog, k = 2, metric = "manhattan")
+pam_clusters3 <- pam(x = reglog, k = 3, metric = "manhattan")
+pamk2.plot <- fviz_cluster(object = pam_clusters2, data = reglog, ellipse.type = "t",
+                           repel = TRUE) +
+  theme_bw() +
+  labs(title = "Resultados clustering PAM") +
+  theme(legend.position = "none")
+
+pamk3.plot <- fviz_cluster(object = pam_clusters3, data = reglog, ellipse.type = "t",
+                           repel = TRUE) +
+  theme_bw() +
+  labs(title = "Resultados clustering PAM") +
+  theme(legend.position = "none")
+ggarrange(pamk2.plot,pamk3.plot)
 
 
 pam_clusters <- pam(x = data, k = 2, metric = "euclidean")
